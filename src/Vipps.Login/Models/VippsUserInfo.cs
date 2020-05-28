683bf801864408bb697fa1073ca7bedab61c2790
Brian Weeteling <brianweeteling@gmail.com>
2020-05-28 10:37:23 +0200

    Add UserInfo model

---
+++ src/Vipps.Login/Models/VippsUserInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityModel;
+using Newtonsoft.Json;
+
+namespace Vipps.Login.Models
+{
+
+    public class VippsUserInfo
+    {
+        public Guid Sub { get; set; }
+
+        [JsonProperty(JwtClaimTypes.Address)]
+        public IEnumerable<VippsAddress> Addresses { get; set; }
+        
+        [JsonProperty(JwtClaimTypes.BirthDate)]
+        public DateTime? BirthDate { get; set; }
+
+        public string Email { get; set; }
+        
+        [JsonProperty(JwtClaimTypes.EmailVerified)]
+        public bool? EmailVerified { get; set; }
+        
+        [JsonProperty(JwtClaimTypes.FamilyName)]
+        public string FamilyName { get; set; }
+        
+        [JsonProperty(JwtClaimTypes.GivenName)]
+        public string GivenName { get; set; }
+        
+        public string Name { get; set; }
+        
+        [JsonProperty(JwtClaimTypes.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        public string Nnin { get; set; }
+    }
+}
