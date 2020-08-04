4303413f6f58a6b36bdb926b979cb7c0710e52fa
Brian Weeteling <brianweeteling@gmail.com>
2020-08-04 17:37:53 +0200

    Add exception in case linking account fails

---
+++ src/Vipps.Login.Episerver.Commerce/Exceptions/VippsLoginLinkAccountException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vipps.Login.Episerver.Commerce.Exceptions
+{
+    public class VippsLoginLinkAccountException : Exception
+    {
+        public VippsLoginLinkAccountException(string message)
+            : base(message)
+        {
+        }
+    }
+}
